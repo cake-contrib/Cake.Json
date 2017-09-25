d91915bbf07fc466e0440c4d8501510bd3011fbd
Jonathan Dick <jondick@gmail.com>
2017-09-25 09:14:13 -0400

    Migrate to Recipe.Cake, xunit, new csproj format

--- Cake.Json.Tests/FakeContext.cs
+++ Cake.Json.Tests/FakeContext.cs
@@ -15,9 +15,7 @@ namespace Cake.Json.Tests
 
         public FakeCakeContext ()
         {
-            testsDir = new DirectoryPath (
-                System.IO.Path.GetFullPath(
-                    System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "../../")));
+            testsDir = new DirectoryPath(System.IO.Path.GetFullPath(AppContext.BaseDirectory));
 
             var environment = Cake.Testing.FakeEnvironment.CreateUnixEnvironment (false);
 
